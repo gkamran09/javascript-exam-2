2aad3396979b4fb9f0a8dbbfd33830053f9fda6c
Gerard Kamran Kalambacal <kamrangerardkalambacal@gmail.com>
2023-08-04 17:52:30 +0800

    Added all the implementation for the test cases per exercise

--- src/exercise-2.js
+++ src/exercise-2.js
@@ -1,5 +1,8 @@
 const getTheExactNumber = (numbers) => {
-  // implement code here
+
+  const multiplesOf3 = numbers.filter((number) => number % 3 === 0);
+  multiplesOf3.sort((a, b) => b - a);
+  return multiplesOf3[0];
 }
 
 export default getTheExactNumber;
